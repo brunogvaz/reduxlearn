83d313fa6a57424600b5e22062500e9a95760566
brunogvaz <brunogvaz@hotmail.com>
2019-05-19 07:40:58 -0300

    fim módulo 9

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { directive } from "@babel/types";
+
+
+
+const AboutPage =()=>(
+
+<div>
+
+  <h2>About</h2>
+  <p>This app uses React, Redux, React Router, and ....</p>
+</div>
+  
+);
+
+export default AboutPage;
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+
+import {bindActionCreators} from 'redux';
+
+class CoursesPage extends Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleEvent = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.actions.createCourse(this.state.course);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleEvent}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </form>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return { courses: state.courses };
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    //createCourse: course=> dispatch(courseActions.createCourse(course))};
+
+    actions : bindActionCreators(courseActions,dispatch)
+  };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CoursesPage);
